8d28d06faeb2b82e2a40222dfe5f5b66ac06c5ec
marian.husar <marian.husar@eset.sk>
2019-11-15 14:31:44 +0100

    add integration test for show post handler

--- tests/Unit/ShowPostHandlerTest.php
+++ tests/Unit/ShowPostHandlerTest.php
@@ -2,9 +2,15 @@
 
 namespace Tests\Unit;
 
+use App\Authentication\AuthenticatedContext;
 use App\Authentication\Token\TokenAuthenticator;
 use App\Http\Action\ShowPostHandler;
+use App\Post\Censor\PostWithTrimmedBodyLocker;
+use App\Post\Contract\Post;
+use App\Post\Policy\PostPolicy;
+use App\Post\Provider\CensoredPostProvider;
 use App\Post\Provider\PostProvider;
+use App\Post\Repository\PostRepository;
 use App\Post\View\PostView;
 use App\User\Contract\User;
 use PHPUnit\Framework\TestCase;
@@ -25,8 +31,40 @@ class ShowPostHandlerTest extends TestCase
             ->willReturn($user);
 
         $handler = new ShowPostHandler($postProvider, $authenticator);
-        $result = $handler->showPost(12345);
+        $result  = $handler->showPost(12345);
 
         $this->assertSame($postView, $result);
     }
+    
+    public function testCanGetCensoredPost()
+    {
+        $postId = 789;
+
+        $post = $this->getMockBuilder(Post::class)->getMock();
+        $post->method('getTitle')
+            ->willReturn('This is title');
+        $post->method('getBody')
+            ->willReturn('this is body of post');
+
+        $postRepository = $this->getMockBuilder(PostRepository::class)->getMock();
+        $postRepository
+            ->method('getPost')
+            ->with($this->equalTo($postId))
+            ->willReturn($post);
+
+        $repository = $postRepository;
+        $policy = new PostPolicy();
+        $locker = new PostWithTrimmedBodyLocker();
+
+        $authenticator = new AuthenticatedContext();
+        $provider = new CensoredPostProvider($repository, $policy, $locker);
+
+
+        $handler = new ShowPostHandler($provider, $authenticator);
+
+        $result = $handler->showPost(789);
+
+        $this->assertInstanceOf(PostView::class, $result);
+    }
+
 }
